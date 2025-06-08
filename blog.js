fdeaf221b007144a9272666202a641f914886f3e
unknown <albertkungu290@gmail.com>
2025-06-08 05:33:25 +0300

    blog done

--- blog.js
+++ blog.js
@@ -307,3 +307,5 @@ const tag = document.createElement('script');
 tag.src = "https://www.youtube.com/iframe_api";
 const firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+
